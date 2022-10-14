050ebc3ba6a8220f5edbb28137fe3cabe3c3addb
Kaegan Davis Haidome <obichi@coseo.com>
2022-10-14 05:19:31 +0100

    funcpoint

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,16 @@
-i
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name:name to print
+ * @f: function which print name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
